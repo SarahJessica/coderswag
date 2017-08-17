40a819480cb7373e621692951d229d6c6c71feb7
Jessica Leivers <sarahjessicaleivers@gmail.com>
2017-08-17 12:55:13 +0100

    initial

---
+++ coderswag/coderswag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coderswag
+//
+//  Created by Jess Leivers on 17/08/2017.
+//  Copyright © 2017 Sarah Jessica London. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
