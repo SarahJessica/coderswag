4de53200aa254330a2286676294202ba8078f39f
Jessica Leivers <sarahjessicaleivers@gmail.com>
2017-08-17 13:23:31 +0100

    initial UI with tableview

---
+++ coderswag/coderswag/View/CategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryCell.swift
+//  coderswag
+//
+//  Created by Jess Leivers on 17/08/2017.
+//  Copyright © 2017 Sarah Jessica London. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
