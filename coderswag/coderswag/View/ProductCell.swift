f21e1f13bc6373f68f18b7ea5cceedf96c308218
Jessica Leivers <sarahjessicaleivers@gmail.com>
2017-08-18 23:03:06 +0100

    adds collection view layout for products

---
+++ coderswag/coderswag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coderswag
+//
+//  Created by Jess Leivers on 18/08/2017.
+//  Copyright © 2017 Sarah Jessica London. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
